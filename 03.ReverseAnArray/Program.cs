a62e471d4b3f0f58ef5d5775e95969c5afd71800
Teodor Kostov <148477548+rebelwithoutacause@users.noreply.github.com>
2025-03-05 08:43:14 +0200

    Add files via upload

---
+++ 03.ReverseAnArray/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+int n = int.Parse(Console.ReadLine());
+
+int[] numbers = new int[n];
+for (int i = 0; i < n; i++) 
+{
+int currNum = int.Parse(Console.ReadLine());
+
+    numbers[i] = currNum;
+}
+
+int[] reverseArray = new int[n];
+
+for(int i = numbers.Length - 1; i >=0; i--) 
+{
+reverseArray[numbers.Length - i - 1] = numbers[i];
+}
+Console.WriteLine(string.Join(" ", reverseArray));
