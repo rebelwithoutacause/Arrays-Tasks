c231f841a83bda38da6db4653f26a36404ee58d6
Teodor Kostov <148477548+rebelwithoutacause@users.noreply.github.com>
2025-03-05 08:42:17 +0200

    Add files via upload

---
+++ 02.SumAnArray/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] numbersArray = Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+int sum = 0;
+
+for (int i = 0; i < numbersArray.Length; i++) 
+{
+    sum += numbersArray[i];
+}
+Console.WriteLine(sum);
